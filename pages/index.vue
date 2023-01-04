ee81a3f76090682015398f6d15930b2fd0615950
994AK <1505398065@qq.com>
2023-01-05 00:35:26 +0800

    添加meta-Head配置 SEO

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <a-button type="primary" @click="info">
-      Display normal message
-    </a-button>
+    <p>服务器名字:HuaYu</p>
+    <p>服务器版本 {{ data.version }}</p>
+    <p>在线玩家: {{ data.players.list.join(',') }}</p>
   </div>
 </template>
 
 <script setup>
-import { message } from 'ant-design-vue'
-const info = () => {
-  message.info('This is a normal message')
-}
+const { data } = await getData('api/minecraft/state')
+useHead({
+  title: 'HuaYu服务器',
+  // or, instead:
+  // titleTemplate: (title) => `My App - ${title}`,
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'HuaYu服务器', content: '我的世界服务器1.19.2' },
+    { hid: 'keywords', name: 'keywords', content: '我的世界服务器, HuaYu,我的世界1.19.2' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
+  ]
+})
 </script>
